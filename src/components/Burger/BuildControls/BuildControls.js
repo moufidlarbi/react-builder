de7e5083a795a508ad9b1449455d07a08bfa2b56
Moufid Larbi <moufidlarbi@gmail.com>
2018-04-16 22:59:20 -0400

    added build control

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from 'BuildControls.css';
+import BuildControl from 'BuildControl';
+
+const buildControls = (props) => (
+  <div>
+
+  </div>
+);
+
+export default buildControls;
