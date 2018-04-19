e3cb144bf103bb78500f38a133db6061758721d3
Moufid Larbi <moufidlarbi@gmail.com>
2018-04-19 18:28:29 -0400

    added order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -90,13 +92,16 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Aux>
+				<Modal>
+					<OrderSummary ingredients={this.state.ingredients} />
+				</Modal>
 				<Burger ingredients={ this.state.ingredients }/>
 				<BuildControls
 					ingredientAdded={ this.addIngredientHandler }
 					ingredientRemove={ this.removeIngredientHandler}
 					disabled={ disabledInfo }
 					purchaseable={ this.state.purchaseable }
-					totalPrice={ this.state.totalPrice }/>
+					totalPrice={ this.state.totalPrice } />
 			</Aux>
 		);
 	}
