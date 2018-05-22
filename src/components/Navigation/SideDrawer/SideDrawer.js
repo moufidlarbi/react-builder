e83b2f0fc87467f10d903057cdf3136272b566e4
Moufid Larbi <moufidlarbi@gmail.com>
2018-05-22 16:31:13 -0400

    added menu toggle

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -11,6 +11,7 @@ const sideDrawer = ( props ) => {
     if (props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
+    
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
