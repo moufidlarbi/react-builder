900d2ca9590123f49d5d2698ac14e9c630512c41
Moufid Larbi <moufidlarbi@gmail.com>
2018-05-28 23:30:24 -0400

    removed interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,19 +8,27 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         state={
             error: null
         }
+
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error:null});
                 return req;
             });
 
-            axios.interceptors.response.use(null, error => {
+            this.resInterceptor = axios.interceptors.response.use(null, error => {
                 this.setState({error: error});
             });
         }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
         errorConfirmedHandler = () => {
             this.setState({error: null})
         }
+
         render() {
             return (
                 <Aux>
