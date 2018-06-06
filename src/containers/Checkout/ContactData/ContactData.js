941e6b8a072b3b89821df475f72df5dc5bf5dd88
Moufid Larbi <moufidlarbi@gmail.com>
2018-06-05 22:53:05 -0400

    fixed error

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -86,6 +86,7 @@ class ContactData extends Component {
                     {value: 'cheapest', displayValue: 'Cheapest'}
                 ]
             },
+            validation: {},
             value: '',
             valid: true
         }
